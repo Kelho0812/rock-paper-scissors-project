667b6c8cd7156c6798f0c0d7e9bb62cd9a4ee070
Kelho <Kelho0812@hotmail.com>
2021-10-30 00:33:41 +0100

    Made a Loop to play the game 5x Created a scoreboard Added some Comments

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,3 +1,11 @@
+
+// First we create 2 variables to store the score of the Player & the score of the Computer and set them both to 0
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Now we create a function to determine the play from the Computer. We do so by creating 3 choices, then making a random choice between the 3 and then returning the value.
+
 function computerPlay() {
     let choiceArray = ["Rock", "Paper", "Scissors"];
     let randomChoice =
@@ -5,33 +13,57 @@ function computerPlay() {
     return randomChoice
 }
 
-function playRound(playerSelection, computerSelection) {
-    let capitalized = caseInsensitive(playerSelection);
-
-    if ((capitalized == "Rock" && computerSelection == "Paper") ||
-        (capitalized == "Scissors" && computerSelection == "Rock") ||
-        (capitalized == "Paper" && computerSelection == "Scissors")) {
-        console.log(`You Lose! ${computerSelection} beats ${capitalized}`)
-    }
-    else if ((capitalized == "Rock" && computerSelection == "Scissors") ||
-        (capitalized == "Scissors" && computerSelection == "Paper") ||
-        (capitalized == "Paper" && computerSelection == "Rock")) {
-        console.log(`You Win! ${capitalized} beats ${computerSelection}`)
-
-    }
-    else {
-        console.log(`Its a tie! ${capitalized} ties with ${computerSelection} `)
-    }
-}
-
-function caseInsensitive(playerSelection) {
-    let lowerCase = playerSelection.toLowerCase();
+function caseInsensitive(Word) {
+    let lowerCase = Word.toLowerCase();
     let firstLetter = lowerCase[0].toUpperCase();
     let restWord = lowerCase.substr(1);
     let fullWord = `${firstLetter}` + `${restWord}`
     return fullWord
 }
-const playerSelection = "ROCK";
-const computerSelection = computerPlay();
-console.log(caseInsensitive(playerSelection));
-console.log(playRound(playerSelection, computerSelection));
+
+function playerSelection() {
+    let capitalized = caseInsensitive(prompt("Make your selection", "Rock"));
+    return capitalized
+}
+
+function playRound(play1, play2) {
+    let computerWins = `You Lose! ${play2} beats ${play1}`;
+    let playerWins = `You Win! ${play1} beats ${play2}`;
+    let tie = `Its a tie! ${play1} ties with ${play2}`;
+    if ((play1 == "Rock" && play2 == "Paper") ||
+        (play1 == "Scissors" && play2 == "Rock") ||
+        (play1 == "Paper" && play2 == "Scissors")) {
+        computerScore++
+        return computerWins + ` Computer Score - ${computerScore} <==> ${playerScore} - Player Score`
+    }
+    else if ((play1 == "Rock" && play2 == "Scissors") ||
+        (play1 == "Scissors" && play2 == "Paper") ||
+        (play1 == "Paper" && play2 == "Rock")) {
+        playerScore++
+        return playerWins + ` Computer - ${computerScore} <==> ${playerScore} - Player`
+    }
+    else {
+        return tie + ` Computer Score - ${computerScore} <==> ${playerScore} - Player Score`
+    }
+}
+
+function Game() {
+    let gameLost = `You LOST the game! Computer wins ${computerScore} - ${playerScore}!`
+    let gameWon = `You WON the game! Player wins ${playerScore} - ${computerScore}!`
+    let gameTied = `It's a tie! The result is ${playerScore} - ${computerScore}!`
+
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection(), computerPlay()));
+    }
+    if (computerScore > playerScore) {
+        console.log((`You LOST the game! Computer wins ${computerScore} - ${playerScore}!`))
+    }
+
+    else if (computerScore < playerScore) {
+        console.log((`You WON the game! Player wins ${playerScore} - ${computerScore}!`))
+    }
+
+    else {
+        console.log((`It's a tie! The result is ${playerScore} - ${computerScore}!`))
+    }
+}
