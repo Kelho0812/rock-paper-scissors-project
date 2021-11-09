b8218491243772669a5d13eeb4f750a77e640c96
Kelho0812 <Jorge0812@gmail.com>
2021-11-09 09:32:51 +0000

    Created some :hover: and :onclick: events

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,4 +1,3 @@
-
 // First we create 2 variables to store the score of the Player & the score of the Computer and set them both to 0
 
 let playerScore = 0;
@@ -10,40 +9,51 @@ function computerPlay() {
     let choiceArray = ["Rock", "Paper", "Scissors"];
     let randomChoice =
         choiceArray[Math.floor(Math.random() * choiceArray.length)];
-    return randomChoice
+    return randomChoice;
 }
 
 function caseInsensitive(Word) {
     let lowerCase = Word.toLowerCase();
     let firstLetter = lowerCase[0].toUpperCase();
     let restWord = lowerCase.substr(1);
-    let fullWord = `${firstLetter}` + `${restWord}`
-    return fullWord
+    let fullWord = `${firstLetter}` + `${restWord}`;
+    return fullWord;
 }
 
 function playerSelection() {
     let capitalized = caseInsensitive(prompt("Make your selection", "Rock"));
-    return capitalized
+    return capitalized;
 }
 
 function playRound(play1, play2) {
     let computerWins = `You Lose! ${play2} beats ${play1}`;
     let playerWins = `You Win! ${play1} beats ${play2}`;
     let tie = `Its a tie! ${play1} ties with ${play2}`;
-    if ((play1 == "Rock" && play2 == "Paper") ||
+    if (
+        (play1 == "Rock" && play2 == "Paper") ||
         (play1 == "Scissors" && play2 == "Rock") ||
-        (play1 == "Paper" && play2 == "Scissors")) {
-        computerScore++
-        return computerWins + ` Computer Score - ${computerScore} <==> ${playerScore} - Player Score`
-    }
-    else if ((play1 == "Rock" && play2 == "Scissors") ||
+        (play1 == "Paper" && play2 == "Scissors")
+    ) {
+        computerScore++;
+        return (
+            computerWins +
+            ` Computer Score - ${computerScore} <==> ${playerScore} - Player Score`
+        );
+    } else if (
+        (play1 == "Rock" && play2 == "Scissors") ||
         (play1 == "Scissors" && play2 == "Paper") ||
-        (play1 == "Paper" && play2 == "Rock")) {
-        playerScore++
-        return playerWins + ` Computer - ${computerScore} <==> ${playerScore} - Player`
-    }
-    else {
-        return tie + ` Computer Score - ${computerScore} <==> ${playerScore} - Player Score`
+        (play1 == "Paper" && play2 == "Rock")
+    ) {
+        playerScore++;
+        return (
+            playerWins +
+            ` Computer - ${computerScore} <==> ${playerScore} - Player`
+        );
+    } else {
+        return (
+            tie +
+            ` Computer Score - ${computerScore} <==> ${playerScore} - Player Score`
+        );
     }
 }
 
@@ -52,14 +62,40 @@ function Game() {
         console.log(playRound(playerSelection(), computerPlay()));
     }
     if (computerScore > playerScore) {
-        console.log((`You LOST the game! Computer wins ${computerScore} - ${playerScore}!`))
-    }
-
-    else if (computerScore < playerScore) {
-        console.log((`You WON the game! Player wins ${playerScore} - ${computerScore}!`))
-    }
-
-    else {
-        console.log((`It's a tie! The result is ${playerScore} - ${computerScore}!`))
+        console.log(
+            `You LOST the game! Computer wins ${computerScore} - ${playerScore}!`
+        );
+    } else if (computerScore < playerScore) {
+        console.log(
+            `You WON the game! Player wins ${playerScore} - ${computerScore}!`
+        );
+    } else {
+        console.log(
+            `It's a tie! The result is ${playerScore} - ${computerScore}!`
+        );
     }
 }
+
+const images = document.querySelectorAll("img");
+
+images.forEach((img) => {
+    // and for each one we add a 'click' listener
+    img.addEventListener("mouseenter", () => {
+        img.style.transform = "scale(1.1)";
+    });
+
+    img.addEventListener("mouseleave", () => {
+        img.style.transform = "scale(1.0)";
+    });
+
+    img.addEventListener("mousedown", () => {
+        // img.style.borderWidth = "5px";
+        img.style.borderColor = "#fff";
+    });
+    img.addEventListener("mouseup", () => {
+        img.style.borderWidth = "";
+        img.style.borderColor = "";
+    });
+
+    img.addEventListener("click", playRound)
+});
